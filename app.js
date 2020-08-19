f88721318ca496c01282dae81486de99e8c15fe7
Bishal Giri <bishal.giri@neesum.com>
2020-08-19 14:19:54 +0545

    message removed from app.js, implemented sum function

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/awesome/feature", (req,res,next)=>{
-	res.send({"success":true, msg: "hello world from awesome feature"});
+	res.send({"success":true});
 });
 
 router.get("/main", (req,res,next)=>{
